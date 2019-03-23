0eca2e1daf0e155de1a213855b3f213865eea0dd
Chenna Reddy <chenna@alavala.in>
2019-03-23 22:31:42 +0100

    currency symbols

--- lib/src/banking/currency.dart
+++ lib/src/banking/currency.dart
@@ -9,8 +9,19 @@ class Currency {
   /**
    * Valid Currencies
    */
-  static Set<String> currenciesSupported = Set.of([INR, EUR, USD, GBP, AUD, JPY]);
+  static final Set<String> currenciesSupported = Set.of([INR, EUR, USD, GBP, AUD, JPY]);
 
+
+  /**
+   * Valid Currencies
+   */
+  static final Map<String, String> currencySymbols = {
+    INR: '\u{20B9}',
+    EUR: '\u{20AC}',
+    USD: '\u{0024}',
+    GBP: '\u{00A3}',
+    JPY: '\u{00A5}',
+  };
   /**
    * Check if the given currency is valid
    *
@@ -19,5 +30,9 @@ class Currency {
    */
   static bool isValidCurrency(String currency) {
     return currency != null && currenciesSupported.contains(currency);
+  }
+
+  static String currencySymbol(String currency) {
+    return currencySymbols[currency] ?? currency;
   }
 }
